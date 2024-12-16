f237a28cb15b1b1813ef129b8308c242f9133739
ChoiHyunSan <chsan626@gmail.com>
2024-12-16 12:20:45 +0900

    create backend project

---
+++ backend/rest-article-site/src/main/java/com/ll/restarticlesite/RestArticleSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ll.restarticlesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestArticleSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestArticleSiteApplication.class, args);
+    }
+
+}
